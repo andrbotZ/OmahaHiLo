d56c1e508006b973262910dc1a311f673816ea66
pocht <andrbot@gmail.com>
2021-05-12 06:27:32 +0300

    read arguments added

--- OmahaHiLo/OmahaHiLo.cpp
+++ OmahaHiLo/OmahaHiLo.cpp
@@ -1,20 +1,38 @@
 // OmahaHiLo.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <iostream>
+ 
 #include "OmahaIO.h"
 #include "OmahaBoard.h"
 #include <string>
 #include <vector>
+#include<iostream>
+ 
 
+using namespace std;
 
-
-int main()
+int main(int argc, char** argv)
 {
     OmahaIO omahoIO;
     OmahaBoard omahaBoard;
+    string directory;
+    string inputFile;
+    string outputFile;
 
-    bool isOpened = omahoIO.OpenFile("C:/Users/pocht/Desktop/jobs/iGates/OmahaHiLo/input.txt");
+    directory = argv[0];
+    inputFile = argv[1];
+
+
+  //  directory = directory.substr(0, directory.find_last_of("\\/"));
+    cout << directory << endl;
+    std::replace(directory.begin(), directory.end(), '\\', '\/');
+ //   inputFile = directory + '/' + inputFile;
+    outputFile = argv[2];
+ //   outputFile = directory + '/' + outputFile;
+  //  std::replace(outputFile.begin(), outputFile.end(), '\\', '/');
+    
+
+    bool isOpened = omahoIO.OpenFile(inputFile);
     if (isOpened) {
         omahoIO.ReadFile();
         omahoIO.CloseFile();
@@ -23,10 +41,11 @@ int main()
         omahaBoard.DealCards(omahoIO.getData());
         omahaBoard.Play();
         vector<string> lines = omahaBoard.PrintResults();
-        omahoIO.SaveResultsInFile(lines, "C:/Users/pocht/Desktop/jobs/iGates/OmahaHiLo/output.txt");
+        omahoIO.SaveResultsInFile(lines, outputFile);
     }
    
 
    
 }
+ 
 
